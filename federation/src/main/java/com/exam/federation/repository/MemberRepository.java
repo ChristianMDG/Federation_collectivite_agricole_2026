3fed622caf4ae8f599b62d9b7549c2693a2e9aa6
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-04-21 21:22:03 +0300

    chore(database) update table member

--- federation/src/main/java/com/exam/federation/repository/MemberRepository.java
+++ federation/src/main/java/com/exam/federation/repository/MemberRepository.java
@@ -5,6 +5,10 @@ import com.exam.federation.entity.Member;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -13,6 +17,17 @@ public class MemberRepository {
     private final DataSource dataSource;
 
     public List<Member> createMembers(List<Member> members) {
+        List<Member> createdMembers = new ArrayList<>();
+    String createMemberQuery = """
+            
+            """;
 
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(createMemberQuery)) {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return createdMembers;
     }
 }
--- federation/src/main/resources/data.sql
+++ federation/src/main/resources/data.sql
@@ -3,7 +3,7 @@ create type gender_type as enum('MALE', 'FEMALE');
 create type member_occupation_type as enum('JUNIOR', 'SENIOR', 'SECRETARY', 'TREASURER', 'VICE_PRESIDENT', 'PRESIDENT');
 
 create table member(
-    id serial primary key,
+    id varchar primary key,
     firstname varchar(255) not null,
     lastname varchar(255) not null,
     birthday date,
@@ -12,7 +12,10 @@ create table member(
     profession varchar(255),
     phone_number int,
     email varchar(255),
-    occupation member_occupation_type
+    occupation member_occupation_type,
+    registration_fee_paid BOOLEAN,
+    membership_dues_paid BOOLEAN,
+    collectivity_id VARCHAR
 );
 
 create table collectivity(
