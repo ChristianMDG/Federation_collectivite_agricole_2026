c19e0e00f528731f2c89f4983b5a91144ea5e349
Lahatranomena <hei.lahatra.3@gmail.com>
2026-04-21 16:11:21 +0300

    feat: add data.sql

---
+++ federation/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+create type gender_type as enum('MALE', 'FEMALE');
+
+create type member_occupation_type as enum('JUNIOR', 'SENIOR', 'SECRETARY', 'TREASURER', 'VICE_PRESIDENT', 'PRESIDENT');
+
+create table member(
+    id serial primary key,
+    firstname varchar(255) not null,
+    lastname varchar(255) not null,
+    birthday date,
+    gender gender_type,
+    address varchar(255),
+    profession varchar(255),
+    phone_number int,
+    email varchar(255),
+    occupation member_occupation_type
+);
+
+create table collectivity(
+    id serial primary key,
+    localisation varchar(255)
+
+)
