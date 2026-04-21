69767ed31a6d3d471b01e5e470204e1695b518a6
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-04-21 20:35:02 +0300

    feat(DataSource) change system.env into dtoenv

--- federation/src/main/java/com/exam/federation/config/DataSource.java
+++ federation/src/main/java/com/exam/federation/config/DataSource.java
@@ -4,30 +4,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DataSource {
-            private final Dotenv dotenv;
-             public DataSource() {
-                this.dotenv = Dotenv.configure()
-                        .ignoreIfMissing()
-                        .load();
-            }
-            public Connection getConnection() {
+
+    private final Dotenv dotenv;
+
+    public DataSource() {
+        this.dotenv = Dotenv.load();
+    }
+
+    public Connection getConnection() {
+        try {
+            String jdbcURL = dotenv.get("JDBC_URL");
+            String user = dotenv.get("USERNAME");
+            String password = dotenv.get("PASSWORD");
+
+            return DriverManager.getConnection(jdbcURL, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
             try {
-                String jdbcURl = System.getenv("JDBC_URl");
-                String user = System.getenv("USERNAME");
-                String password = System.getenv("PASSWORD");
-                return DriverManager.getConnection(jdbcURl, user, password);
+                connection.close();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
-        public void closeConnection(Connection connection) {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
     }
 }
--- federation/src/main/java/com/exam/federation/repository/MemberRepository.java
+++ federation/src/main/java/com/exam/federation/repository/MemberRepository.java
@@ -1,4 +1,18 @@
 package com.exam.federation.repository;
 
+import com.exam.federation.config.DataSource;
+import com.exam.federation.entity.Member;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
 public class MemberRepository {
+    private final DataSource dataSource;
+
+    public List<Member> createMembers(List<Member> members) {
+
+    }
 }
