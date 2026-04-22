35be0f645d86416a68902b66dea21572d1165474
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-04-22 18:28:33 +0300

    feat(database) : add IF NOT EXISTS to all the table

--- federation/src/main/resources/data.sql
+++ federation/src/main/resources/data.sql
@@ -3,7 +3,7 @@ create type member_occupation_type as enum ('JUNIOR', 'SENIOR', 'SECRETARY', 'TR
 CREATE TYPE frequency_type AS ENUM ('WEEKLY', 'MONTHLY', 'ANNUALLY', 'PUNCTUALLY');
 CREATE TYPE activity_status_type AS ENUM ('ACTIVE', 'INACTIVE');
 
-create table member
+create table IF NOT EXISTS member
 (
     id                    varchar primary key,
     firstname             varchar(255) not null,
@@ -19,14 +19,14 @@ create table member
     membership_dues_paid  BOOLEAN,
     collectivity_id       VARCHAR
 );
-CREATE TABLE member_referees
+CREATE TABLE IF NOT EXISTS member_referees
 (
     member_id  VARCHAR REFERENCES member (id),
     referee_id VARCHAR REFERENCES member (id),
     PRIMARY KEY (member_id, referee_id)
 );
 
-create table collectivity
+create table IF NOT EXISTS collectivity
 (
     id                varchar(255) primary key,
     location          varchar(255),
@@ -40,14 +40,14 @@ ALTER TABLE collectivity
     ADD COLUMN number VARCHAR(50) UNIQUE,
     ADD COLUMN name   VARCHAR(255) UNIQUE;
 
-CREATE TABLE collectivity_members
+CREATE TABLE IF NOT EXISTS collectivity_members
 (
     collectivity_id VARCHAR(255) REFERENCES collectivity (id) ON DELETE CASCADE,
     member_id       VARCHAR(255) REFERENCES member (id) ON DELETE CASCADE,
     PRIMARY KEY (collectivity_id, member_id)
 );
 
-CREATE TABLE membership_fee
+CREATE TABLE IF NOT EXISTS membership_fee
 (
     id              VARCHAR(255) PRIMARY KEY,
     collectivity_id VARCHAR(255) REFERENCES collectivity (id),
@@ -57,7 +57,7 @@ CREATE TABLE membership_fee
     label           VARCHAR(255),
     status          activity_status_type DEFAULT 'ACTIVE'
 );
-CREATE TABLE membership_fee
+CREATE TABLE  IF NOT EXISTS membership_fee
 (
     id              VARCHAR PRIMARY KEY,
     collectivity_id VARCHAR NOT NULL REFERENCES collectivity (id) ON DELETE CASCADE,
@@ -68,7 +68,7 @@ CREATE TABLE membership_fee
     status          activity_status_type
 );
 
-CREATE TABLE collectivity_transaction
+CREATE TABLE IF NOT EXISTS collectivity_transaction
 (
     id                VARCHAR(255) PRIMARY KEY,
     collectivity_id   VARCHAR(255) REFERENCES collectivity (id),
@@ -80,6 +80,19 @@ CREATE TABLE collectivity_transaction
     created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE SEQUENCE member_id_seq START 1000;
-create sequence collectivity_id_seq start 2000;
-CREATE SEQUENCE membership_fee_id_seq START 3000;
+CREATE TABLE IF NOT EXISTS collectivity_transaction
+(
+    id                VARCHAR(255) PRIMARY KEY,
+    collectivity_id   VARCHAR(255) REFERENCES collectivity (id),
+    member_id         VARCHAR(255) REFERENCES member (id),
+    membership_fee_id VARCHAR(255) REFERENCES membership_fee (id),
+    amount            DECIMAL(15, 2) NOT NULL,
+    payment_mode      VARCHAR(50)    NOT NULL,
+    transaction_date  DATE           NOT NULL,
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE SEQUENCE IF NOT EXISTS member_id_seq START 1000;
+CREATE SEQUENCE IF NOT EXISTS collectivity_id_seq start 2000;
+CREATE SEQUENCE IF NOT EXISTS membership_fee_id_seq START 3000;
+CREATE SEQUENCE IF NOT EXISTS transaction_id_seq START 1000;
