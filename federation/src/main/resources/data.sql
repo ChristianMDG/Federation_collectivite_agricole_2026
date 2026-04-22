dd7916402bd6486ddac300ee396a02cf47b41dc0
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-04-22 18:19:59 +0300

    feat(database) : arrange data

--- federation/src/main/resources/data.sql
+++ federation/src/main/resources/data.sql
@@ -1,5 +1,4 @@
 create type gender_type as enum ('MALE', 'FEMALE');
-
 create type member_occupation_type as enum ('JUNIOR', 'SENIOR', 'SECRETARY', 'TREASURER', 'VICE_PRESIDENT', 'PRESIDENT');
 CREATE TYPE frequency_type AS ENUM ('WEEKLY', 'MONTHLY', 'ANNUALLY', 'PUNCTUALLY');
 CREATE TYPE activity_status_type AS ENUM ('ACTIVE', 'INACTIVE');
@@ -37,6 +36,10 @@ create table collectivity
     secretary_id      varchar(255) references member (id)
 );
 
+ALTER TABLE collectivity
+    ADD COLUMN number VARCHAR(50) UNIQUE,
+    ADD COLUMN name   VARCHAR(255) UNIQUE;
+
 CREATE TABLE collectivity_members
 (
     collectivity_id VARCHAR(255) REFERENCES collectivity (id) ON DELETE CASCADE,
@@ -54,36 +57,29 @@ CREATE TABLE membership_fee
     label           VARCHAR(255),
     status          activity_status_type DEFAULT 'ACTIVE'
 );
+CREATE TABLE membership_fee
+(
+    id              VARCHAR PRIMARY KEY,
+    collectivity_id VARCHAR NOT NULL REFERENCES collectivity (id) ON DELETE CASCADE,
+    eligible_from   DATE,
+    frequency       frequency_type,
+    amount          NUMERIC(10, 2),
+    label           VARCHAR(255),
+    status          activity_status_type
+);
 
-ALTER TABLE collectivity
-    ADD COLUMN number VARCHAR(50) UNIQUE,
-    ADD COLUMN name   VARCHAR(255) UNIQUE;
-
+CREATE TABLE collectivity_transaction
+(
+    id                VARCHAR(255) PRIMARY KEY,
+    collectivity_id   VARCHAR(255) REFERENCES collectivity (id),
+    member_id         VARCHAR(255) REFERENCES member (id),
+    membership_fee_id VARCHAR(255) REFERENCES membership_fee (id),
+    amount            DECIMAL(15, 2) NOT NULL,
+    payment_mode      VARCHAR(50)    NOT NULL,
+    transaction_date  DATE           NOT NULL,
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE SEQUENCE member_id_seq START 1000;
 create sequence collectivity_id_seq start 2000;
-CREATE SEQUENCE IF NOT EXISTS membership_fee_id_seq START 1000;
-
-
-CREATE TYPE frequency_type AS ENUM (
-    'MONTHLY',
-    'YEARLY'
-    );
-
-CREATE TYPE activity_status_type AS ENUM (
-    'ACTIVE',
-    'INACTIVE'
-    );
-
-CREATE TABLE membership_fee (
-                                id               VARCHAR PRIMARY KEY,
-                                collectivity_id  VARCHAR NOT NULL REFERENCES collectivity(id) ON DELETE CASCADE,
-                                eligible_from    DATE,
-                                frequency        frequency_type,
-                                amount           NUMERIC(10,2),
-                                label            VARCHAR(255),
-                                status           activity_status_type
-);
-
 CREATE SEQUENCE membership_fee_id_seq START 3000;
-
