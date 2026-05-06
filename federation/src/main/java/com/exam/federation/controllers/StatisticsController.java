04db87d498b4c95ecfa0500b50cd090e88faef54
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-05-07 00:53:18 +0300

    handler error

--- federation/src/main/java/com/exam/federation/controllers/StatisticsController.java
+++ federation/src/main/java/com/exam/federation/controllers/StatisticsController.java
@@ -1,5 +1,6 @@
 package com.exam.federation.controllers;
 
+import com.exam.federation.Exception.BusinessException;
 import com.exam.federation.dto.CollectivityLocalStatistics;
 import com.exam.federation.services.StatisticsService;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -7,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StatisticsController {
@@ -23,7 +26,15 @@ public class StatisticsController {
             @PathVariable String id,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
-        List<CollectivityLocalStatistics> statistics = statisticsService.getLocalStatistics(id, from, to);
-        return ResponseEntity.ok(statistics);
+
+        try {
+            List<CollectivityLocalStatistics> statistics = statisticsService.getLocalStatistics(id, from, to);
+            return ResponseEntity.ok(statistics);
+        } catch (BusinessException e) {
+            Map<String, Object> error = new HashMap<>();
+            error.put("status", e.getStatusCode());
+            error.put("message", e.getMessage());
+            return ResponseEntity.status(e.getStatusCode()).body(error);
+        }
     }
 }
